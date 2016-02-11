ac1d80c6039c78c907534162b17edbb930cd1b51
joostkouwenhoven <joostkouwenhoven@gmail.com>
2016-02-11 16:18:29 +0100

    Update OpenApp.scala

--- src/test/scala/com/gatling/demo/gatling/useCases/OpenApp.scala
+++ src/test/scala/com/gatling/demo/gatling/useCases/OpenApp.scala
@@ -13,7 +13,8 @@ object OpenApp  {
         exec(
             http("Open Page")
             .get("/")
-            .check(headerRegex("Set-Cookie", "XSRF-TOKEN=(.*?);").saveAs("csrf_token"))
+            .check(headerRegex("Set-Cookie", "XSRF-TOKEN=(.?*);").saveAs("csrf_token"))
+            .check(headerRegex("Set-Cookie", "SESSION=(.?*);").saveAs("session"))
             .headers(Map("Accept" -> """application/json"""))
              )
      
@@ -22,24 +23,25 @@ object OpenApp  {
             http("Log In")
 			.post("/api/login")
             .basicAuth("admin","admin")
-            .headers(Map("XSRF-TOKEN" -> "${csrf_token}"))
+            .headers(Map("XSRF-TOKEN" -> "${csrf_token}", "SESSION" -> "${session}"))
+            .check(headerRegex("Set-Cookie", "SESSION=(.?*);").saveAs("session"))
             )
         exec(
             http("Confirm Login")           
             .get("/app/components/app/app.html")
-            .headers(Map("XSRF-TOKEN" -> "${csrf_token}"))
-            .check(headerRegex("Set-Cookie", "XSRF-TOKEN=(.*?);").saveAs("csrf_token"))
+            .headers(Map("XSRF-TOKEN" -> "${csrf_token}", "SESSION" -> "${session}"))
+            .check(headerRegex("Set-Cookie", "XSRF-TOKEN=(.?*);").saveAs("csrf_token"))
              )
 	val useCase3 = 
   exec(http("Get Projects")
 		.get("/api/projects?limit=5&page=1")
-        .headers(Map("XSRF-TOKEN" -> "${csrf_token}"))
+        .headers(Map("XSRF-TOKEN" -> "${csrf_token}", "SESSION" -> "${session}"))
         )       
         
      	val useCase4 = 
   exec(http("Log Out")
 		.post("/api/logout")
-        .headers(Map("XSRF-TOKEN" -> "${csrf_token}"))
+        .headers(Map("XSRF-TOKEN" -> "${csrf_token}", "SESSION" -> "${session}"))
         .check(status.is(401)))
 
 }
